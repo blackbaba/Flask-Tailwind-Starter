10f8e6b74321ff5d22308262d76180da3b962511
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2021-01-11 17:57:24 +1100

    Add Config for PROD-DEV-TEST loaded based on FLASK_ENV

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,14 @@ from flask import Flask
 # load_dotenv('.env')
 
 app = Flask(__name__)
-if os.environ.get('FLASK_ENV') == 'production':
-    app.config.from_object('config.ProductionConfig')
+
+# Import configuration profile based on FLASK_ENV variable - defaults to Production
+if os.environ.get('FLASK_ENV') == 'development':
+    app.config.from_object('config.DevelopmentConfig')
 elif os.environ.get('FLASK_ENV') == 'testing':
     app.config.from_object('config.TestingConfig')
 else:
-    app.config.from_object('config.DevelopmentConfig')
+    app.config.from_object('config.ProductionConfig')
+
 
 from app.routes import *
