3ec38f955d66a1b43e9917ba803f03788b759e10
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2021-01-11 16:50:41 +1100

    Enable purge using NODE_ENV variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: ((process.env.NODE_ENV === 'production') ? true : false),
     content: ['./app/templates/**/*.html']
   },
   presets: [],
