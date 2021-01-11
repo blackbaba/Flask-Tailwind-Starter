c845cc6ea71d1b50e81b534a00597ba995e1e4d6
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2021-01-11 18:01:54 +1100

    routes rename

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,13 @@ import datetime as dt
 from flask import render_template, url_for
 from app import app
 from app.helpers import *
-
+from app.forms import *
+from app.models import *
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/environment')
-def environment():
-    return f"NODE ENV: {os.environ.get('NODE_ENV')} and FLASK ENV: {os.environ.get('FLASK_ENV')} and Configuration profile for: {app.config.get('TEST_CONFIG')}"
+@app.route('/system')
+def system():
+    return f"NODE ENV: {os.environ.get('NODE_ENV')},  FLASK ENV: {os.environ.get('FLASK_ENV')} and Configuration profile for: {app.config.get('TEST_CONFIG')}"
--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from app import app
 
 if __name__ == "__main__":
     app.config['ENV']="development"
-    app.config['DEBUG']=1
     app.run()
